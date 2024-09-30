879afa3a6de369b43bc1f182f6e02ecb7ebdab31
Siimas <simaomarques20@gmail.com>
2024-09-30 22:40:36 +0100

    first commit

---
+++ src/main/java/com/trackit/bug_service/BugServiceApplication.java
@@ -0,0 +1,13 @@
+package com.trackit.bug_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BugServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trackit/bug_service/BugServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trackit.bug_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
